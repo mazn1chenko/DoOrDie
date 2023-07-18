68ab9bf68343109250a2f77cd24704b859821b52
mazn1chenko <maznichenko.144@gmail.com>
2023-07-18 17:18:46 +0300

    FirstCommit
    FirstCommit

---
+++ DoOrDie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DoOrDie
+//
+//  Created by m223 on 18.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = .lightGray
+        setup()
+    }
+    
+    func setup() {
+        
+        print("I`m running")
+    }
+
+
+}
+
